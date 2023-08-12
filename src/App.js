f25a3a897ba090c0c52bfad86d184cbc7f70cc58
SeungUk Ahn <ahn2802@gmail.com>
2023-08-12 17:32:37 +0900

    Counter 컴포넌트 property 설정

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import Counter from "./components/counter/Counter";
 function App() {
   return (
     <div className="App">
-      <Counter/>
+        <Counter by={1}/>
+        <Counter by={2}/>
+        <Counter by={3}/>
     </div>
   );
 }
+
+// function PlayWithProps({property1, property2}){
+//     console.log(property1)
+//     console.log(property2)
+//     return(
+//         <div>
+//             Props
+//         </div>
+//     )
+// }
 
 export default App;
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,12 +1,17 @@
 import './Counter.css'
 import {useState} from "react";
+import PropTypes from "prop-types";
 
-export default function Counter(){
+export default function Counter({by}){
 
     const [count, setCount] = useState(0);
 
     function incrementCounterFunction(){
-        setCount(count + 1)
+        setCount(count + by)
+    }
+
+    function decrementCounterFunction(){
+        setCount(count - by)
     }
 
     return(
@@ -15,8 +20,19 @@ export default function Counter(){
             <div>
                 <button className="counterButton"
                         onClick={incrementCounterFunction}
-                >+1</button>
+                >+{by}</button>
+                <button className="counterButton"
+                        onClick={decrementCounterFunction}
+                >-{by}</button>
             </div>
         </div>
     )
+}
+
+Counter.propTypes = {
+    by: PropTypes.number
+}
+
+Counter.defaultProps = {
+    by: 1
 }
