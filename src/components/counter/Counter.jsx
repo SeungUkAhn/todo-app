735a407db4619997ad41044647d2499b0c8b67ce
SeungUk Ahn <ahn2802@gmail.com>
2023-08-12 17:14:23 +0900

    Counter 컴포넌트에 state 추가

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,14 +1,17 @@
 import './Counter.css'
+import {useState} from "react";
 
 export default function Counter(){
 
+    const [count, setCount] = useState(0);
+
     function incrementCounterFunction(){
-        console.log('increment clicked')
+        setCount(count + 1)
     }
 
     return(
         <div className="Counter">
-            <span className="count">0</span>
+            <span className="count">{count}</span>
             <div>
                 <button className="counterButton"
                         onClick={incrementCounterFunction}
