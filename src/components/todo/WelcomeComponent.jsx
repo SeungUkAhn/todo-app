00c1f7198610298ccc08b02f9a1f17a27929c7c4
SeungUk Ahn <ahn2802@gmail.com>
2023-08-14 22:11:08 +0900

    axios 설치, helloworld API 호출 예제 작성

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,14 +1,34 @@
 import {Link, useParams} from "react-router-dom";
+import axios from "axios";
 
 export default function WelcomeComponent(){
 
     const {username} = useParams()
+
+    function callHelloWorldRestApi(){
+        alert('called');
+        axios.get('http://localhost:8080/hello-world')
+            .then((response) => successResponse(response))
+            .catch((response) => errorResponse(response))
+            .finally(() => console.log('cleanup'))
+    }
+
+    function successResponse(response){
+        console.log(response)
+    }
+
+    function errorResponse(response){
+        console.log(response)
+    }
 
     return(
         <div className="Welcome">
             <h1>Welcome! {username}</h1>
             <div>
                 <Link to="/todos">할 일 목록으로 이동</Link>
+            </div>
+            <div>
+                <button class="btn btn-success m-5" onClick={callHelloWorldRestApi}>헬로월드 API 호출</button>
             </div>
         </div>
     )
