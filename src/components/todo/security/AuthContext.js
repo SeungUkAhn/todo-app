52fcbc9db127a3fdbe72a284145540dbf5a3859c
SeungUk Ahn <ahn2802@gmail.com>
2023-08-14 19:14:12 +0900

    AuthContext - state 업데이트 기능, useAuth hook 작성

--- src/components/todo/security/AuthContext.js
+++ src/components/todo/security/AuthContext.js
@@ -1,13 +1,17 @@
-import {createContext, useState} from "react";
+import {createContext, useContext, useState} from "react";
 
 //컨텍스트 생성하기
 export const AuthContext = createContext()
+
+export const useAuth = () => useContext(AuthContext)
 
 //다른 컴포넌트들과 생성된 컨택스트를 공유하기
 export default function AuthProvider({children}){
 
     //컨텍스트에 상태 부여
     const [number, setNumber] = useState(10)
+
+    setInterval(() => setNumber(number + 1), 10000)
 
     return(
         <AuthContext.Provider value={{number}}>
